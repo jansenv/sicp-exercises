2706cfd8bfe3f563f22a1feeeec6b877b58c4020
Jansen <jansenv44@gmail.com>
2020-05-24 22:28:47 -0500

    1.9 revised

--- 1.9.scm
+++ 1.9.scm
@@ -5,9 +5,35 @@
       b
       (inc (+ (dec a) b))))
 
+;linear recursion
+
+;(inc (+ (dec 4) 5))
+;(inc (+ (3) 5))
+;(inc (inc (+ (dec 3) 5)))
+;(inc (inc (+ (2) 5)))
+;(inc (inc (inc (+ (dec 2) 5))))
+;(inc (inc (inc (+ (1) 5))))
+;(inc (inc (inc (inc (+ (dec 1) 5)))))
+;(inc (inc (inc (inc (+ (0) 5)))))
+;(inc (inc (inc (inc 5))))
+;(inc (inc (inc 6)))
+;(inc (inc 7))
+;(inc (8))
+;9
+
 (define (+ a b)
   (if (= a 0)
       b
       (+ (dec a) (inc b))))
 
-;There is no recursion going on at all in either of these examples. You can simply substitute a & b and, in the first exmaple, the counter will decrement a, add it to b, and return b incremented (unless a is 0). In example 2 it is similar except a will be decremented at the same time b is incremented, and then they are added together. I suppose these are example of linear iteration.
+;iteration
+
+;(+ (dec 4) (inc 5))
+;(+ 3 6)
+;(+ (dec 3) (inc 6))
+;(+ 2 7)
+;(+ (dec 2) (inc 7))
+;(+ 1 8)
+;(+ (dec 1) (inc 8))
+;(+ 0 9)
+;9
