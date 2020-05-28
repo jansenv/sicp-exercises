90ec2421816ef51d41224113921f975043477e27
Jansen <jansenv44@gmail.com>
2020-05-27 20:35:40 -0500

    wip

---
+++ 1.10.scm
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10)
+
+(A 1 0)
+
+(A 2 4)
+
+(A 3 3)
+
+; (f n) = 2n || 0 if n = 0
+; (g n) = 2^n || 0 if n = 0
+; (h n) = 
