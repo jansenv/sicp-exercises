5d48e5f8d3853e61e28932bfecc7d946d86ac812
Jansen <jansenv44@gmail.com>
2020-05-29 20:27:26 -0500

    1.11

--- 1.11.scm
+++ 1.11.scm
@@ -1,6 +1,13 @@
 #lang racket
-(define (f n)
-  (cond ((< n 3) n)
-        (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+;(define (f n)
+;  (cond ((< n 3) n)
+;        (else (+ (f (- n 1))
+;           (* 2 (f (- n 2)))
+;           (* 3 (f (- n 3)))))))
 
-(f 2)
+(define (f n) (f-iter n 0 1 2))
+
+(define (f-iter i a b c)
+  (cond ((< i 0) i)
+        ((= i 0) a)
+        (else (f-iter (- i 1) b c (+ c (* 2 b) (* 3 a))))))
