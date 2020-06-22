149469419d08cfab11014e32b7a9649220a08174
Jansen <jansenv44@gmail.com>
2020-06-21 21:48:26 -0500

    1.20

---
+++ 1.20.scm
@@ -0,0 +1,54 @@
+#lang racket
+
+;Normal-order: 18 remainder operations
+
+;(gcd 206 40) 
+  
+;(if (= 40 0) ...) 
+  
+;(gcd 40 (remainder 206 40)) 
+  
+;(if (= (remainder 206 40) 0) ...) 
+  
+;(if (= 6 0) ...) 
+  
+;(gcd (remainder 206 40) (remainder 40 (remainder 206 40))) 
+  
+;(if (= (remainder 40 (remainder 206 40)) 0) ...) 
+  
+;(if (= 4 0) ...) 
+  
+;(gcd (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 
+  
+;(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) ...) 
+  
+;(if (= 2 0) ...) 
+  
+;(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))) 
+  
+;(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0) ...) 
+  
+;(if (= 0 0) ...) 
+;(remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+
+;Applicative-order: 4 remainder operations
+
+;(gcd 206 40) 
+  
+;(gcd 40 (remainder 206 40)) 
+  
+;(gcd 40 6) 
+  
+;(gcd 6 (remainder 40 6)) 
+  
+;(gcd 6 4) 
+  
+;(gcd 4 (remainder 6 4)) 
+  
+;(gcd 4 2) 
+  
+;(gcd 2 (remainder 4 2)) 
+  
+;(gcd 2 0) 
+  
+;2 
