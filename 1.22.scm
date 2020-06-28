7492124ec832152cc4d0a29c31b2d3c78a1d66bc
Jansen <jansenv44@gmail.com>
2020-06-28 18:07:15 -0500

    1.22

--- 1.22.scm
+++ 1.22.scm
@@ -3,12 +3,40 @@
 (define (timed-prime-test n)
   (newline)
   (display n)
-  (start-prime-test n (runtime)))
+  (start-prime-test n (current-milliseconds)))
 
 (define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime (- (runtime) start-time))))
+  (cond (prime? n)
+      (report-prime (- (current-milliseconds) start-time))))
 
-(define (report-prime elapsed-time)
+(define (report-prime n elapsed-time)
+  (newline)
+  (display n)
   (display " *** ")
   (display elapsed-time))
+
+(define (square x) (* x x))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n ) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (search-for-primes lower upper)
+  (define (iter n)
+    (cond ((<= n upper) (timed-prime-test n) (iter (+ n 2)))))
+  (iter (if (odd? lower) lower (+ lower 1))))
+
+(search-for-primes 1000 1019)
+(search-for-primes 10000 10037)
+(search-for-primes 100000 100043)
+(search-for-primes 1000000 1000037)
