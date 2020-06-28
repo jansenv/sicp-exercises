5283ea2141d6232cc24178163f7c2562dc84f34a
Jansen <jansenv44@gmail.com>
2020-06-28 18:19:10 -0500

    testing again

--- 1.22.scm
+++ 1.22.scm
@@ -1,19 +1,8 @@
 #lang racket
 
-(define (timed-prime-test n)
-  (newline)
-  (display n)
-  (start-prime-test n (current-inexact-milliseconds)))
+(define (runtime) (current-inexact-milliseconds)) ; adapting to DrRacket
 
-(define (start-prime-test n start-time)
-  (cond (prime? n)
-      (report-prime (- (current-inexact-milliseconds) start-time))))
-
-(define (report-prime n elapsed-time)
-  (newline)
-  (display n)
-  (display " *** ")
-  (display elapsed-time))
+; --- Prime computation ---
 
 (define (square x) (* x x))
 
@@ -21,9 +10,13 @@
   (find-divisor n 2))
 
 (define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n ) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
+  (cond ((> (square test-divisor) n)
+         n)
+        ((divides? test-divisor n)
+         test-divisor)
+        (else (find-divisor
+               n
+               (+ test-divisor 1)))))
 
 (define (divides? a b)
   (= (remainder b a) 0))
@@ -31,12 +24,35 @@
 (define (prime? n)
   (= n (smallest-divisor n)))
 
-(define (search-for-primes lower upper)
-  (define (iter n)
-    (cond ((<= n upper) (timed-prime-test n) (iter (+ n 2)))))
-  (iter (if (odd? lower) lower (+ lower 1))))
+; --- Timing ---
 
-(search-for-primes 1000 1019)
-(search-for-primes 10000 10037)
-(search-for-primes 100000 100043)
-(search-for-primes 1000000 1000037)
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))
+      "nothing"))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start-range end-range)
+  (if (even? start-range)
+      (search-for-primes (+ 1 start-range) end-range)
+      (cond ((> start-range end-range)
+             (newline) (display "done"))
+            (else (timed-prime-test start-range)
+                  (search-for-primes (+ 2 start-range) end-range)))))
+
+(search-for-primes 1000 1090)
+(search-for-primes 10000 10090)
+(search-for-primes 100000 100090)
+(search-for-primes 1000000 1000090)
+(search-for-primes 10000000 10000090)
+(search-for-primes 100000000 100000090)
+(search-for-primes 1000000000 1000000090)
+(search-for-primes 10000000000 10000000090)
