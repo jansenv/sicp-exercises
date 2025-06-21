af5f10e1757c4b3d36a2b2f571f3c13fe7617e9d
Jansen van der Spuy <jansenv44@gmail.com>
2025-06-21 13:18:15 -0500

    returning to this book after 5 years

--- 1.06.scm
+++ 1.06.scm
@@ -1,5 +1,4 @@
-#lang racket
+#lang simply-scheme
 
-; In Scheme, conditionals such as if and cond are SPECIAL FORMS. This is in contrast to procedures, in this case new-if, which will be run in applicative order. In the conditional special forms the predicate is evaluated first, and then the interpreter either evaluates the consequent or the alternative.
-
-; However what we are using (the 'new if') is just a regular procedure. In applicative order this means that it has to evaluate all of the arguments before it runs. In essence it is infinitely recursive, because the two arguments it has to evaluate are 'guess' and the '(sqr-iter (improve guess x) x)))'. The latter argument is called every time new-if is called, resulting in an infinite loop.
+; we have to rememember 'if' and 'cond' are special forms, 'new-if' is not going to be one so it will have to evaluate every argument that is input instead of stopping on the first true predicate.
+; this makes it vulnerable to getting stuck in a recursive loop, so the program will likely crash.
