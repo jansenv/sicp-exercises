e2c3e5b130656739b7d3c04bfd45bad857c90168
Jansen van der Spuy <jansenv44@gmail.com>
2025-06-29 19:58:54 -0500

    4

---
+++ cs61a/week2/labs/lab2.scm
@@ -0,0 +1,26 @@
+#lang simply-scheme
+
+;2
+(define (substitute sent oldWord newWord)
+  (cond ((empty? sent)'())
+        ((equal? (first sent) oldWord)
+         (se newWord (substitute (bf sent) oldWord newWord)))
+        (else
+         (se (first sent) (substitute (bf sent) oldWord newWord)))))
+
+;3
+;g has one argument and the type returned is an integer
+
+;4
+(define f 5)
+f
+(define (f1) 42)
+(f1)
+(define (f2 x) (* x 2))
+(f2 3)
+(define (f3) (lambda () 'test))
+((f3))
+(define (f4) (lambda () (lambda (x) (+ x 1))))
+(((f4)) 3)
+
+;5
